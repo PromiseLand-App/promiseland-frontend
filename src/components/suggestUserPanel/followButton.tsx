61a112d45e3a98ac2e1e20d6c89ec17cdccfc225
Adi <adi-li@users.noreply.github.com>
2022-09-24 22:53:08 -0400

    Fix lenshub proxy address

--- src/components/suggestUserPanel/followButton.tsx
+++ src/components/suggestUserPanel/followButton.tsx
@@ -1,20 +1,24 @@
 import { useState } from 'react';
-import { useContractWrite, usePrepareContractWrite } from 'wagmi';
+import { chain, useContractWrite, usePrepareContractWrite } from 'wagmi';
 
 import LensHubProxy from '@/abis/LensHubProxy.json';
+import useChainId from '@/hooks/useChainId';
 import IProfile from '@/schemas/profile';
-import { LENSHUB_PROXY_ADDRESS } from '@/utils/constants';
+import { LENSHUB_PROXY_ADDRESS_MAP } from '@/utils/constants';
 
 interface IProps {
   profile: IProfile;
 }
 
 const FollowButton = ({ profile }: IProps) => {
+  const chainId = useChainId();
   const [loading, setLoading] = useState(false);
   const [buttonText, setButtonText] = useState('Follow');
 
   const { config } = usePrepareContractWrite({
-    addressOrName: LENSHUB_PROXY_ADDRESS,
+    addressOrName:
+      LENSHUB_PROXY_ADDRESS_MAP[chainId] ??
+      LENSHUB_PROXY_ADDRESS_MAP[chain.polygonMumbai.id],
     contractInterface: LensHubProxy,
     functionName: 'follow',
     args: [[profile.id], [0x0]],
