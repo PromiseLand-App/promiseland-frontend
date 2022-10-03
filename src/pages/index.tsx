7b9f45a176fd2e0ccc705c23b18fffce5bf6d745
brucewang <brucewangyq@gmail.com>
2022-10-03 02:24:52 -0400

    feat: add hero

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,8 +17,38 @@ const Home: NextPage = () => {
   }, [isConnected, router]);
 
   return (
-    <Layout hasSidebar>
-      <div className="space-y-3 lg:mx-0">Promiseland</div>
+    <Layout>
+      <section className="">
+        <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:items-center">
+          <div className="mx-auto max-w-3xl text-center">
+            <h1 className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl">
+              Your content your NFT
+              <span className="sm:block ">Your content is valueable</span>
+            </h1>
+
+            <p className="mx-auto mt-4 max-w-xl sm:text-xl sm:leading-relaxed">
+              PromiseLand is social NFT marketplace which can benefit both
+              content creators and their supporters.
+            </p>
+
+            <div className="mt-8 flex flex-wrap justify-center gap-4">
+              <a
+                className="block w-full rounded border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-black focus:outline-none focus:ring active:text-opacity-75 sm:w-auto"
+                href="/get-started"
+              >
+                Connect your WalletGet Started
+              </a>
+
+              <a
+                className="block w-full rounded border border-blue-600 px-12 py-3 text-sm font-medium text-black hover:bg-blue-600 focus:outline-none focus:ring active:bg-blue-800 sm:w-auto"
+                href="/about"
+              >
+                Explore
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
     </Layout>
   );
 };
